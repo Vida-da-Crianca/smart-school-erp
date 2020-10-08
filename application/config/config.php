c0c9943cbaca0229c9a1824ed5d4f0871a1bd6c9
Carlos Carvalho <carloscarvalho@MacBook-Pro-de-Carlos.local>
2020-10-08 13:51:20 -0300

    - Add  language br - configure docker stack dev

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   | a PHP script and you can easily do that on your own.
   |
  */
-$config['base_url'] = '';
+$config['base_url'] = 'http://erp.dev.in/';
 
 
 /*
@@ -515,4 +515,4 @@ $config['rewrite_short_tags'] = FALSE;
 $config['routine_session'] = 16;
 $config['routine_update'] = 1529539200;
 $config['proxy_ips'] = '';
-$config['installed'] = false;
+$config['installed'] = true;
--- application/config/license.php
+++ application/config/license.php
@@ -3,6 +3,6 @@ if (!defined('BASEPATH')) {
     exit('No direct script access allowed');
 }
 
-$config['envato_market_purchase_code'] = '';
+$config['envato_market_purchase_code'] = '1882912';
 $config['envato_market_username']      = '';
-$config['SSLK'] = '';
+$config['SSLK'] = '63AH2N-XTFFLP-952QAM-UTBUai9NL0Q5R2lLeE4xbXZBMGVMVnEwMlpvTTU0QmNFcml6emVLNkk0MD0=';
