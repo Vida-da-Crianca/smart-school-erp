85be517b60fd1e62fa2396a58ffccb233a3f43e7
carlosocarvalho <contato@carlosocarvalho.com.br>
2020-10-08 22:35:32 -0300

    add base url in app

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   | a PHP script and you can easily do that on your own.
   |
  */
-$config['base_url'] = 'http://erp.dev.in/';
+$config['base_url'] = getenv('BASE_URL');
 
 
 /*
