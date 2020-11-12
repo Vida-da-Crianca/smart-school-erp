ad9bff5988b18d59493665abc3cb94df8287d39d
carlosocarvalho <contato@carlosocarvalho.com.br>
2020-11-11 21:45:21 -0300

    change gitignore

--- application/logs/log-2020-11-04.php
+++
@@ -1,2 +0,0 @@
-ERROR - 2020-11-04 20:58:20 --> Severity: error --> Exception: Property [id] does not exist on this collection instance. /app/vendor/illuminate/support/Collection.php 2160
-ERROR - 2020-11-04 20:58:58 --> Severity: error --> Exception: Property [id] does not exist on this collection instance. /app/vendor/illuminate/support/Collection.php 2160
