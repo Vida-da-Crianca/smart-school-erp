bde5ef345881dc35010aa7bf294c42c7779ad74a
bistocchi <68290948+bistocchi@users.noreply.github.com>
2021-01-10 17:58:21 -0300

    Add files via upload

--- application/views/admin/onlinestudent/studentEdit.php
+++ application/views/admin/onlinestudent/studentEdit.php
@@ -770,12 +770,12 @@
                         $('#guardian_name').val($('#father_name').val());
                         $('#guardian_phone').val($('#father_phone').val());
                         $('#guardian_occupation').val($('#father_occupation').val());
-                        $('#guardian_relation').val("Pai")
+                        $('#guardian_relation').val("Father")
                     } else if (value == "mother") {
                         $('#guardian_name').val($('#mother_name').val());
                         $('#guardian_phone').val($('#mother_phone').val());
                         $('#guardian_occupation').val($('#mother_occupation').val());
-                        $('#guardian_relation').val("Mãe")
+                        $('#guardian_relation').val("Mother")
                     } else {
                         $('#guardian_name').val("");
                         $('#guardian_phone').val("");
