fd513c8d977e3fc70789db68f65bf20208954163
carlosocarvalho <contato@carlosocarvalho.com.br>
2021-02-22 13:58:07 -0300

    change script for cancel invoices on group on billets

--- application/command/CancelBillet.php
+++ application/command/CancelBillet.php
@@ -2,7 +2,7 @@
 
 namespace Application\Command;
 
-
+use Illuminate\Database\Eloquent\Collection;
 use Packages\Commands\BaseCommand;
 
 
@@ -40,7 +40,7 @@ class CancelBillet extends BaseCommand
         }
 
 
-        $billets = \Billet_eloquent::onlyTrashed()->where('is_active', 1)->get()->groupBy('bank_bullet_id');
+        $billets = \Billet_eloquent::onlyTrashed()->with(['invoices'])->where('is_active', 1)->get()->groupBy('bank_bullet_id');
 
 
         foreach ($billets as $item) {
@@ -51,9 +51,10 @@ class CancelBillet extends BaseCommand
                 $row->save();
                 continue;
             }
+
             $this->CI->bank_payment_inter->cancel(
                 ['number' => $row->bank_bullet_id, 'motive' => $row->status],
-                function ($status) use ($row) {
+                function ($status) use ($row, $item) {
                     if ($status->success) {
                         \Billet_eloquent::onlyTrashed()
                             ->where('bank_bullet_id', $row->bank_bullet_id)
@@ -61,13 +62,26 @@ class CancelBillet extends BaseCommand
                                 'is_active' => 0,
                             ]);
                     }
-                    \Invoice_eloquent::where('bullet_id', $row->bank_bullet_id)
-                        ->update(['status' => \Invoice_eloquent::PENDING_DELETE]);
+                    $this->cancelAllInvoice($item);
+
+                    // \Invoice_eloquent::where('bullet_id', $row->bank_bullet_id)
+                    //     ->update(['status' => \Invoice_eloquent::PENDING_DELETE]);
 
                     discord_log(sprintf('%s', json_encode(array_merge(['bolelo' => $row->bank_bullet_id ], (array) $status), JSON_PRETTY_PRINT)), 'Cancelamento de Boleto');
                 }
             );
         }
         return 0;
+    }
+
+
+
+    private function cancelAllInvoice(Collection $items): void
+    {
+        foreach ($items as $billet) {
+            $billet->invoices->each(function ($invoice) {
+                $invoice->update(['status' => \Invoice_eloquent::PENDING_DELETE]);
+            });
+        }
     }
 }
