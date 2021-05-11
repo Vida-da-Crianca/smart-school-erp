d710120fe39215967cbc85c21edc5d8d3e7924ec
bistocchi <68290948+bistocchi@users.noreply.github.com>
2021-05-11 11:36:21 -0300

    Add files via upload

--- application/controllers/admin/Recibo.php
+++ application/controllers/admin/Recibo.php
@@ -1,27 +1,9 @@
 <?php
-class Recibo extends Admin_Controller {
-
- function index(){
-	  
-	  
-	$data = array(
-		'title'   => 'Recibo',
-		'content' => 'Gerador de Recibo',
-	);
-
-   
-  }
-  
-  
-  
-  function gerar($id) {
-	  
-		$data['dados'] = $query = $this->db->get_where('expenses','expenses.id = '.$id);
-	  
+class Recibo extends CI_Controller {
 	
-       $this->load->view('admin/expense/recibo', $data);
-  }
-  
-  
+	function gerar($id)
+	{
+		$data['dados'] = $query = $this->db->get_where('expenses','expenses.id = '.$id);
+		$this->load->view('admin/expense/recibo', $data);
+	}
 }
-?> 
