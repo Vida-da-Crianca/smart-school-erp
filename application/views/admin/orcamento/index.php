983a8d2fb7a18aad4c316e7c15c18ca5765f1b52
leandro <leandro@leandro-note>
2021-07-08 14:37:52 -0300

    controle de orçamentos 4

--- application/views/admin/orcamento/index.php
+++ application/views/admin/orcamento/index.php
@@ -72,9 +72,9 @@
         </div> <!-- general form elements -->
     </section><!-- Main content -->
 </div>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-<link href="<?php echo base_url();?>backend/bootstrap-dialog/dist/css/bootstrap-dialog.min.css" rel="stylesheet">
-<script src="<?php echo base_url();?>backend/bootstrap-dialog/dist/js/bootstrap-dialog.min.js"></script> 
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js" type="text/javascript"></script>
+<link href="<?php echo base_url();?>backend/bootstrap-dialog/dist/css/bootstrap-dialog.min.css" rel="stylesheet" type="text/css">
+<script src="<?php echo base_url();?>backend/bootstrap-dialog/dist/js/bootstrap-dialog.min.js" type="text/javascript"></script> 
 
 <script type='text/javascript'>
 $(document).ready(function(){
