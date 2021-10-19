215fc9b12c41f0edfe9530fbbc9c51218de2aaa5
Leandro Betin <leandro_btn@hotmail.com>
2021-10-19 08:08:34 -0300

    Update Onlinestudent.php

--- application/controllers/admin/Onlinestudent.php
+++ application/controllers/admin/Onlinestudent.php
@@ -268,14 +268,14 @@ class Onlinestudent extends Admin_Controller {
                     $this->db->where('id',$response->student_id);
                     $this->db->update('online_admissions',['image'=>'uploads/student_images/'.$this->input->post('image')]);
 
-                    $sender_details = array('student_id' => $response->student_id, 'contact_no' => $this->input->post('guardian_phone'), 'email' => $this->input->post('guardian_email'));
+                  /*  $sender_details = array('student_id' => $response->student_id, 'contact_no' => $this->input->post('guardian_phone'), 'email' => $this->input->post('guardian_email'));
                     $this->mailsmsconf->mailsms('student_admission', $sender_details);
 
                     $student_login_detail = array('id' => $response->student_id, 'credential_for' => 'student', 'username' => $this->student_login_prefix . $response->student_id, 'password' => isset($user_password) ? $user_password : '---', 'contact_no' => $this->input->post('mobileno'), 'email' => $this->input->post('email'));
                     $this->mailsmsconf->mailsms('login_credential', $student_login_detail);
 
                     $parent_login_detail = array('id' => $response->student_id, 'credential_for' => 'parent', 'username' => $this->parent_login_prefix . $response->student_id, 'password' =>  isset($parent_password) ? $parent_password : '---', 'contact_no' => $this->input->post('guardian_phone'), 'email' => $this->input->post('guardian_email'));
-                    $this->mailsmsconf->mailsms('login_credential', $parent_login_detail);
+                    $this->mailsmsconf->mailsms('login_credential', $parent_login_detail);*/
                 }
 
                 $this->session->set_flashdata('msg', '<div class="alert alert-success text-left">' . $this->lang->line('update_message') . '</div>');
