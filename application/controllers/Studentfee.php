f423fcbffe82e376f65686f197b41fd86a6eb77d
bistocchi <68290948+bistocchi@users.noreply.github.com>
2021-11-26 10:54:22 -0300

    Update Studentfee.php

--- application/controllers/Studentfee.php
+++ application/controllers/Studentfee.php
@@ -433,7 +433,6 @@ class Studentfee extends Admin_Controller
         foreach ($options->toArray() as $v) {
             $years[$v] = $v;
         }
-	sort($years);
         $data['optionsYear'] = $years;
         $data['current_year'] = $due_date;
       
